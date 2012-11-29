10fd23213fb10064b02b423859b5e1c626da4ec9
Michael Schmidt <michael@embeddedARM.com>
2012-11-28 17:40:00 -0700

    added needed file to project

---
+++ ts/types.h
@@ -0,0 +1,16 @@
+#ifndef __types_h
+#define __types_h
+
+typedef unsigned char uint8;
+typedef unsigned short uint16;
+typedef unsigned int uint32;
+typedef unsigned long long int uint64;
+typedef char int8;
+typedef short int16;
+typedef int int32;
+typedef long long int int64;
+typedef float float32;
+typedef double float64;
+typedef float real32;
+typedef double real64;
+#endif
