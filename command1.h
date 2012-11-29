52747258a83ffaf83f64e7ec88254ae2c2d212d2
Michael Schmidt <michael@embeddedARM.com>
2012-11-28 17:40:59 -0700

    file was missing from git, but is used by tsctl.c

---
+++ command1.h
@@ -0,0 +1,59 @@
+#include "Stream.h"
+#include "LookupRef.h"
+#include "ts/co.h"
+
+int tsctlArgParseInt8(Stream *out,LookupRef* *lu,char *arg);
+int tsctlArgParseInt16(Stream *out,LookupRef* *lu,char *arg);
+int tsctlArgParseInt32(Stream *out,LookupRef* *lu,char *arg);
+int tsctlArgParseArrayInt8(Stream *out,LookupRef* *lu,char *arg) ;
+int tsctlArgParseArrayInt16(Stream *out,LookupRef* *lu,char *arg) ;
+int tsctlArgParseArrayUInt8(Stream *out,LookupRef* *lu,char *arg) ;
+int tsctlArgParseArrayInt32(Stream *out,LookupRef* *lu,char *arg) ;
+int tsctlArgParseUInt8(Stream *out,LookupRef* *lu,char *arg) ;
+int tsctlArgParseUInt16(Stream *out,LookupRef* *lu,char *arg) ;
+int tsctlArgParseUInt32(Stream *out,LookupRef* *lu,char *arg) ;
+int tsctlArgParseReal32(Stream *out,LookupRef* *lu,char *arg) ;
+int tsctlArgParseArrayReal32(Stream *out,LookupRef* *lu,char *arg) ;
+
+int WriteArrayInt8As(coParm,Stream *out,Stream *in,int base,int abase);
+int WriteArrayInt16As(coParm,Stream *out,Stream *in,int base,int abase);
+int WriteArrayUInt8As(coParm,Stream *out,Stream *in,int base,int abase);
+int WriteArrayInt32As(coParm,Stream *out,Stream *in,int base,int abase);
+int WriteArrayUInt32As(coParm,Stream *out,Stream *in,int base,int abase);
+int WriteInt8As(coParm,Stream *out,Stream *in,int base,int abase);
+int WriteInt16As(coParm,Stream *out,Stream *in,int base,int abase);
+int WriteInt32As(coParm,Stream *out,Stream *in,int base,int abase);
+int WriteUInt8As(coParm,Stream *out,Stream *in,int base,int abase);
+int WriteUInt16As(coParm,Stream *out,Stream *in,int base,int abase);
+int WriteUInt32As(coParm,Stream *out,Stream *in,int base,int abase);
+int WriteVoidAs(coParm,Stream *out,Stream *in,int base,int abase);
+int WriteArrayReal32As(coParm,Stream *out,Stream *in,int base,int abase);
+
+int WriteArrayInt8Bin(coParm,Stream *out,Stream *in,int base,int abase);
+int WriteArrayInt16Bin(coParm,Stream *out,Stream *in,int base,int abase);
+int WriteArrayUInt8Bin(coParm,Stream *out,Stream *in,int base,int abase);
+int WriteArrayInt32Bin(coParm,Stream *out,Stream *in,int base,int abase);
+int WriteArrayUInt32Bin(coParm,Stream *out,Stream *in,int base,int abase);
+int WriteInt8Bin(coParm,Stream *out,Stream *in,int base,int abase);
+int WriteInt16Bin(coParm,Stream *out,Stream *in,int base,int abase);
+int WriteInt32Bin(coParm,Stream *out,Stream *in,int base,int abase);
+int WriteUInt8Bin(coParm,Stream *out,Stream *in,int base,int abase);
+int WriteUInt16Bin(coParm,Stream *out,Stream *in,int base,int abase);
+int WriteUInt32Bin(coParm,Stream *out,Stream *in,int base,int abase);
+int WriteVoidBin(coParm,Stream *out,Stream *in,int base,int abase);
+int WriteArrayReal32Bin(coParm,Stream *out,Stream *in,int base,int abase);
+
+int WriteArrayInt8Esc(coParm,Stream *out,Stream *in,int base,int abase);
+int WriteArrayInt16Esc(coParm,Stream *out,Stream *in,int base,int abase);
+int WriteArrayUInt8Esc(coParm,Stream *out,Stream *in,int base,int abase);
+int WriteArrayInt32Esc(coParm,Stream *out,Stream *in,int base,int abase);
+int WriteArrayUInt32Esc(coParm,Stream *out,Stream *in,int base,int abase);
+int WriteInt8Esc(coParm,Stream *out,Stream *in,int base,int abase);
+int WriteInt16Esc(coParm,Stream *out,Stream *in,int base,int abase);
+int WriteInt32Esc(coParm,Stream *out,Stream *in,int base,int abase);
+int WriteUInt8Esc(coParm,Stream *out,Stream *in,int base,int abase);
+int WriteUInt16Esc(coParm,Stream *out,Stream *in,int base,int abase);
+int WriteUInt32Esc(coParm,Stream *out,Stream *in,int base,int abase);
+int WriteVoidEsc(coParm,Stream *out,Stream *in,int base,int abase);
+int WriteArrayReal32Esc(coParm,Stream *out,Stream *in,int base,int abase);
+
