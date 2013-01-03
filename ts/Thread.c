c7297fda83750dd0c4326d09d9f1f5a86e343bee
Michael Schmidt <michael@embeddedARM.com>
2013-01-03 13:53:34 -0700

    conditionally defining some symbols to avoid errors if they are already defined

--- ts/Thread.c
+++ ts/Thread.c
@@ -1,9 +1,15 @@
 #ifndef Thread_C
 #define Thread_C
 #ifdef THREAD_USE_POSIX
+#ifndef __USE_UNIX98
 #define __USE_UNIX98
+#endif
+#ifndef _XOPEN_SOURCE
 #define _XOPEN_SOURCE 600
+#endif
+#ifndef _GNU_SOURCE
 #define _GNU_SOURCE
+#endif
 #include "PThread.c"
 #else
 #include "NoThread.c"
