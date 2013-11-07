81f42eb9474a6cacf063458dc875c6c6e5b76004
Michael Schmidt <michael@embeddedARM.com>
2013-11-06 17:16:18 -0700

    removed obsolete code

--- ts/dioctlConfig.h
+++ ts/dioctlConfig.h
@@ -2,7 +2,6 @@
 #define __dioctlConfig_h
 #include "System.h"
 void dioctl_config_add2(char *names,int len,int lenout);
-void dioctl_config_add(char **names);
 extern NameValuePair **archmapptr;
 extern NameValuePair *archmap;
 #endif
